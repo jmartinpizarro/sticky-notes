76eb1a6f5b2fe39d8b676a07329694eda7cd1531
Javier Martin <jmartinpizarro04@gmail.com>
2024-02-22 16:47:31 +0100

    Fix error and bugs in the code. Removal of useless code.

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,36 @@ function App() {
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
     useEffect(() => {
-        const notesContainer = document.querySelector('.notes-container');
 
-        const obtainPosition = (event) => {
+        const obtainPosition = (event) => { /*for obtaining the x-y position of the mouse*/
             const { clientX, clientY } = event;
             setMousePosition({ x: clientX, y: clientY });
         };
 
         window.addEventListener('mousemove', obtainPosition);
 
-        const handleClick = () => {
-            setNotes([...notes, <Note key={notes.length} posX={mousePosition.x} posY={mousePosition.y} />]);
-        };
-
-        notesContainer.addEventListener('click', handleClick);
-
-        return () => {
+        return () => { /*cleaning function*/
             window.removeEventListener('mousemove', obtainPosition);
-            notesContainer.removeEventListener('click', handleClick);
+
         };
-    }, [notes, mousePosition]);
+
+    }, []);
+
+    const handleSpaceKeyDown = (event) => { /*for adding new notes to the board */
+        if (event.code === "Space") {
+            setNotes([...notes, <Note key={notes.length} posX={mousePosition.x} posY={mousePosition.y} />]);
+        }
+    };
 
     return (
-        <div className='notes-container'>
-            {notes}
+        <div className='notes-container' onKeyDown={handleSpaceKeyDown} tabIndex={0}>
+            {notes.map((note, index) => (
+                <Note
+                    key={index}
+                    posX={note.props.posX}
+                    posY={note.props.posY}
+                />
+            ))}
         </div>
     );
 }
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,18 +1,47 @@
 import PropTypes from 'prop-types';
+import { useRef, useState } from 'react';
 
 function Note({ posX, posY }) {
+    const [isDragging, setIsDragging] = useState(false);
+    const noteRef = useRef(null);
+
+    const handleMouseDown = (event) => {
+        event.preventDefault();
+        setIsDragging(true);
+    };
+
+    const handleMouseMove = (event) => {
+        if (isDragging) {
+            const newX = event.clientX - noteRef.current.offsetWidth / 2;
+            const newY = event.clientY - noteRef.current.offsetHeight / 2;
+            noteRef.current.style.left = newX + 'px';
+            noteRef.current.style.top = newY + 'px';
+        }
+    };
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+    };
+
     return (
-        <div className="note" style={{
-            pointerEvents: 'none',
-            left: posX - 10 + 'px',
-            top: posY - 10 + 'px',
-            width: '100px',
-            height: '100px',
-            backgroundColor: 'yellowgreen',
-            border: '1px solid white',
-            position: 'absolute'
-        }}>
-        </div>
+        <div
+            ref={noteRef}
+            className="note"
+            style={{
+                pointerEvents: 'auto',
+                left: posX + 'px',
+                top: posY + 'px',
+                width: '200px',
+                height: '300px',
+                backgroundColor: 'yellowgreen',
+                border: '1px solid white',
+                position: 'absolute',
+                cursor: isDragging ? 'grabbing' : 'pointer'
+            }}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+        ></div>
     );
 }
 
